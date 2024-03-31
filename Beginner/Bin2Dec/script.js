e189730295423748c96f33a48da57b0ef88a5486
Jonathan Philips <johnphilips.995@gmail.com>
2024-03-31 20:01:12 +0530

    Fixing Enter key issues

--- Beginner/Bin2Dec/script.js
+++ Beginner/Bin2Dec/script.js
@@ -41,6 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     input.addEventListener("keydown", function (event) {
         if (event.key === 'Enter') {
+            event.preventDefault();
             convertBinary();
         }
     });
